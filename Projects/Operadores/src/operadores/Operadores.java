2df90e2772673e4dbabd7ccd686b7509a8f6a0d0
Jhon Part <jhprieto@unal.edu.co>
2025-06-27 21:17:57 -0500

    Operadores done

--- Projects/Operadores/src/operadores/Operadores.java
+++ Projects/Operadores/src/operadores/Operadores.java
@@ -10,15 +10,15 @@ public class Operadores {
     public static void main(String[] args) {
         
         Scanner teclado = new Scanner(System.in);
-        int num1, num2, sum;
+        double num1, num2, sum; // para poder almacenar números flotantes (double)
         
         System.out.println("Ingrese el primer número");
-        num1 = teclado.nextInt();
+        num1 = teclado.nextDouble(); // para recibir números flotantes (nextDouble)
         
         System.out.println("Ingrese el segundo número");
-        num2 = teclado.nextInt();
+        num2 = teclado.nextDouble();
         
-        sum = num1 + num2;
+        sum = num1 / num2;
         System.out.println("El resultado23 es: " + sum);
         
         
