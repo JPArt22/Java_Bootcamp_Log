dbc7fdb0107cc32d50948411376330cc1c49d52a
Jhon Part <jhprieto@unal.edu.co>
2025-06-28 11:09:03 -0500

    If else anidado

--- Projects/Ejercicio_if/src/ejercicio_if/Ejercicio_if.java
+++ Projects/Ejercicio_if/src/ejercicio_if/Ejercicio_if.java
@@ -13,8 +13,25 @@ public class Ejercicio_if {
         System.out.println("Ingrese su edad: ");
         edad = teclado.nextInt();
         
-        if(edad >= 18){
+        if(edad > 18){
             System.out.println("Felicidades, usted es mayor de edad");
+ 
+            if(edad>40){
+                System.out.println("Sos generación X");
+            }
+            else{
+                System.out.println("Sos milenial o centenial");
+            }
+        }
+        
+        else{
+            
+            if (edad == 18){
+                System.out.println("Tienes exactamente 18 años");
+            }
+            else {
+            System.out.println("No puedes acceder, sos menor");
+            }
         }
     }
     
