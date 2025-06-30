fcdd1c514d4e4e51c3a17eaa82781293147b0b89
Jhon Part <jhprieto@unal.edu.co>
2025-06-30 10:17:19 -0500

    Bucle For

---
+++ Projects/Ejemplo_For/src/ejemplo_for/Ejemplo_For.java
@@ -0,0 +1,16 @@
+
+package ejemplo_for;
+
+
+public class Ejemplo_For {
+
+
+    public static void main(String[] args) {
+        
+        for(int contador = 0; contador<10; contador++) {
+            
+            System.out.println("Estoy en la vuelta: "+ contador);
+        }
+    }
+    
+}
