6b2ebbff6cfd2755aad3ae15372fec305b5ec1ff
Jhon Part <jhprieto@unal.edu.co>
2025-07-02 19:58:55 -0500

    some updates

--- Exercises_0/Exercise_4/src/exercise_4/Exercise_4.java
+++ Exercises_0/Exercise_4/src/exercise_4/Exercise_4.java
@@ -7,13 +7,21 @@ public class Exercise_4 {
  
     public static void main(String[] args) {
         
-        int numero = 10;
+        //int numero = 10;
         
-        for(int start = 0; start<numero; start++){
+        /*for(int start = 0; start<numero; start++){
             
             System.out.println(numero-start);
             
+            }*/
+        
+        for(int numero = 10; numero>0; numero--){  // ==> otra opción usando --
+            
+            System.out.println(numero);
+            
             }
+        
+        System.out.println("¡Feliz año nuevo!");
         }
 }
     
