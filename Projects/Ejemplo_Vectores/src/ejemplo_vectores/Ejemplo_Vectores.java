8c50e41b9c07519928528d1105d7cde7327a11b5
Jhon Part <jhprieto@unal.edu.co>
2025-07-05 09:38:30 -0500

    Arrays

---
+++ Projects/Ejemplo_Vectores/src/ejemplo_vectores/Ejemplo_Vectores.java
@@ -0,0 +1,45 @@
+
+package ejemplo_vectores;
+
+import java.util.Scanner;
+
+
+public class Ejemplo_Vectores {
+
+
+    public static void main(String[] args) {
+        
+        int numeros [] = {12, 4, 56,21, 78,90, 32, 1}; // ==> correcto cuando conozco los valores
+        int numeros [] = new int[8];
+        Scanner teclado = new Scanner(System.in);
+        
+        /*
+        numeros[0] = 15;
+        numeros[1] = 8;
+        numeros[2] = 23;
+        numeros[3] = 56;
+        numeros[4] = 12;
+        numeros[5] = 89;
+        numeros[6] = 47;
+        numeros[7] = 91;
+        
+        // mostrar el elemento de la 5ta posición
+        //System.out.println("El elemento de la 5ta posición es: " + numeros[4]);
+        
+    
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println("Ingrese el valor del índice: " + i);
+            numeros[i] = teclado.nextInt();
+            
+        }
+        
+        for (int i = 0; i < numeros.length; i++){
+            
+            System.out.println("Índice: " + i + "Valor: " + numeros[i]);
+            
+        }
+        */
+       
+    }
+    
+}
