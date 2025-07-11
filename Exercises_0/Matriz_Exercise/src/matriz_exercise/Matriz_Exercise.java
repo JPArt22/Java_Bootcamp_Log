0b21bfca4625484ffdabe7d686e5f80326f5d4fd
Jhon Part <jhprieto@unal.edu.co>
2025-07-10 19:51:25 -0500

    Matriz exercise updated

--- Exercises_0/Matriz_Exercise/src/matriz_exercise/Matriz_Exercise.java
+++ Exercises_0/Matriz_Exercise/src/matriz_exercise/Matriz_Exercise.java
@@ -14,8 +14,9 @@ public class Matriz_Exercise {
         double notas[][] = new double[4][3];
         double prom, sum = 0;
         Scanner teclado = new Scanner(System.in);
-        
-        
+        System.out.println("---------------------------------------------");
+        System.out.println("****** Students Qualifications Program ******");
+        System.out.println("---------------------------------------------");
         // for para recorrer la matriz y pedir las notas de cada alumno
         for (int r = 0; r < notas.length; r++){ //recorrer cada fila
             
@@ -25,7 +26,7 @@ public class Matriz_Exercise {
                 notas[r][c] = teclado.nextDouble();
                 
             }
-            
+            System.out.println("---------------------------------------------");
         }
         
         for (int r = 0; r < notas.length; r++){
@@ -37,14 +38,14 @@ public class Matriz_Exercise {
             System.out.println("Las notas del alumno "+ (r+1) + " son: "); // solo se imprime acá para que no se repita siempre
             for (int c = 0; c < notas[1].length; c++){  // aquí recorremos las notas
                 
-                System.out.println(notas[r][c]); //mostramos las 3 notas del alumno r
+                System.out.println(notas[r][c]);    //mostramos las 3 notas del alumno r
                 sum = sum + notas[r][c];            // sumamos todas las notas del alumno r
                 prom = sum / notas[1].length;       // dividimos la suma de todas las notas entre la cantidad de notas
                 
             }
             
             System.out.println("La nota final es: " + prom); 
-                 
+            System.out.println("---------------------------------------------");
             
         }
     }
